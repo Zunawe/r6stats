72d40b6c129fcc4ff7641401056cc27aaa1fa742
Zunawe <gyroscope15@gmail.com>
2020-12-23 02:57:16 -0800

    Remove log files in favor of console
    When running the app as a service, the console logs are already recorded, so there's no need to also save a copy in the same directory as the code. It could also cause issues if the user running the code doesn't have write permissions, which is something only needed for logging

--- server/util/logger.js
+++ server/util/logger.js
@@ -3,12 +3,6 @@ const { createLogger, transports, format } = require('winston')
 const logger = createLogger({
   format: format.printf((info) => `[${info.level.toUpperCase()}] ${info.message}`),
   transports: [
-    new transports.File({
-      filename: './logs/server.log',
-      json: false,
-      maxsize: 5242880,
-      maxFiles: 5
-    }),
     new transports.Console()
   ]
 })
